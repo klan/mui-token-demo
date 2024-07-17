1d5a2f1da5a237faafd0143614cfa2c39e6ad6a1
Klaus Nielsen <kln@impact.dk>
2024-07-17 11:38:20 +0200

    Dynamic icon import

--- src/utils/IconMapper.ts
+++ src/utils/IconMapper.ts
@@ -1,14 +1,10 @@
-import React from 'react';
-import AddIcon from '@mui/icons-material/Add';
-import DeleteIcon from '@mui/icons-material/Delete';
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
-// Import more icons as needed
+import React from "react";
+import dynamic from "next/dynamic";
 
 const iconMap = {
-  add: AddIcon,
-  delete: DeleteIcon,
-  notify: NotificationsActiveIcon
-  // Add more mappings here
+  add: dynamic(() => import("@mui/icons-material/Add")),
+  delete: dynamic(() => import("@mui/icons-material/Delete")),
+  notify: dynamic(() => import("@mui/icons-material/NotificationsActive")),
 };
 
 export type IconNames = keyof typeof iconMap;
