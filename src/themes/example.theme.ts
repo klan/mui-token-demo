3fae307aa996a5d8429b70ebc5d3df40ef7720a3
Klaus Nielsen <kln@impact.dk>
2024-07-21 23:13:21 +0200

    Switched theming on button

--- src/themes/example.theme.ts
+++ src/themes/example.theme.ts
@@ -1,29 +1,67 @@
 import { extendTheme } from "@mui/joy/styles";
 
 export const theme = extendTheme({
-  // cssVarPrefix: 'demo',
+  cssVarPrefix: "demo", // using 'demo' as namespace for css vars
   colorSchemes: {
     light: {
       palette: {
         primary: {
-          // new base colors
-          50: "#fdf2f8",
-          100: "#fce7f3",
-          200: "#fbcfe8",
-          300: "#f9a8d4",
-          400: "#f472b6",
-          500: "#ec4899",
-          600: "#db2777",
-          700: "#be185d",
-          800: "#9d174d",
-          900: "#831843",
-          // new token mapping
-          solidBg: "var(--joy-palette-primary-400)",
-          solidHoverBg: "",
-          solidActiveBg: "var(--joy-palette-primary-500)",
-          outlinedBorder: "var(--joy-palette-primary-500)",
-          outlinedColor: "var(--joy-palette-primary-700)",
-          outlinedActiveBg: "var(--joy-palette-primary-100)",
+          // base colors
+          50: undefined,
+          100: undefined,
+          200: undefined,
+          300: undefined,
+          400: undefined,
+          500: undefined,
+          600: undefined,
+          700: undefined,
+          800: undefined,
+          900: undefined,
+          // solid
+          solidBg: "var(--colorSurfaceBrandBlue4)",
+          solidHoverBg: "var(--colorSurfaceBrandBlue3)",
+          solidActiveBg: "var(--colorSurfaceBrandBlue5)",
+          solidColor: "var(--colorTextInverse)",
+          solidDisabledBg: "var(--colorDisabledSurface)",
+          solidDisabledColor: "var(--colorDisabledText)",
+          solidDisabledBorder: "var(--colorDisabledStroke)",
+          // outline
+          outlinedBg: "var(--colorSurfaceDefault)",
+          outlinedBorder: "var(--colorStrokeBrandBlue2)",
+          outlinedHoverBg: "var(--colorSurfaceBrandBlue1)",
+          outlinedActiveBg: "var(--colorSurfaceBrandBlue2)",
+          outlinedColor: "var(--colorTextBrandBlue2)",
+          outlinedDisabledColor: "var(--colorDisabledText)",
+          outlinedDisabledBorder: "var(--colorDisabledStroke)",
+        },
+        danger: {
+          // base colors
+          50: undefined,
+          100: undefined,
+          200: undefined,
+          300: undefined,
+          400: undefined,
+          500: undefined,
+          600: undefined,
+          700: undefined,
+          800: undefined,
+          900: undefined,
+          // solid
+          solidBg: "var(--colorSystemColorsDanger500)",
+          solidHoverBg: "var(--colorSystemColorsDanger400)",
+          solidActiveBg: "var(--colorSystemColorsDanger900)",
+          solidColor: "var(--colorTextInverse)",
+          solidDisabledBg: "var(--colorDisabledSurface)",
+          solidDisabledColor: "var(--colorDisabledText)",
+          solidDisabledBorder: "var(--colorDisabledStroke)",
+          // outline
+          outlinedBg: "var(--colorSurfaceDefault)",
+          outlinedBorder: "var(--colorSystemColorsDanger500)",
+          outlinedHoverBg: "var(--colorSystemColorsDanger200)",
+          outlinedActiveBg: "var(--colorSystemColorsDanger400)",
+          outlinedColor: "var(--colorSystemColorsDanger500)",
+          outlinedDisabledColor: "var(--colorDisabledText)",
+          outlinedDisabledBorder: "var(--colorDisabledStroke)",
         },
       },
     },
@@ -44,11 +82,24 @@ export const theme = extendTheme({
       },
     },
   },
-  // components: {
-  //   JoyButton: {
-  //     defaultProps: {
-  //       variant: "outlined",
+  // variants: {
+  //   solidDisabled: {
+  //     primary: {
+  //       borderWidth: "1px", // a style property can be added like this, but styleOverrides are often preferred to modify styles
   //     },
   //   },
   // },
+  components: {
+    JoyButton: {
+      styleOverrides: {
+        root: ({ ownerState, theme }) => ({
+          ...(ownerState.disabled &&
+            ownerState.variant === "solid" && {
+              border: "1px solid", // styles can be modified like this on specific variants, etc.
+              // fontSize: theme.vars.fontSize.lg, // css vars are available in theme.vars.*, like eg. 'var(--joy-fontSize-lg)'
+            }),
+        }),
+      },
+    },
+  },
 });
